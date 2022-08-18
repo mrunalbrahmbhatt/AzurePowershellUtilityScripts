6c8717a0841016c64336b97a0161dafbeb4e9ae2
Mrunal Brahmbhatt <it.mrunal@gmail.com>
2022-08-18 22:47:53 +1000

    Create GetTenantAndSubscriptionList.ps1

---
+++ GetTenantAndSubscriptionList.ps1
@@ -0,0 +1,53 @@
+#Purpose : Gets the Tenant and Subscription list in single table.
+
+#Connect-AzAccount
+
+$tenants = Get-AzTenant 
+$subscriptions = Get-AzSubscription | Sort-Object -Property TenantId 
+
+#$tenants | Format-Table -AutoSize
+#$subscriptions | Format-Table -AutoSize
+
+$list = @()
+
+#Combining tenant and subscription details.
+foreach ($t in $tenants)
+{
+
+    foreach ($s in $subscriptions)
+    {
+        # decide whether to join
+        if ($t.Id -eq $s.TenantId)
+        {
+            # output a new object with the join result
+            $list += New-Object PSObject -Property @{
+                TenandId = $t.Id;
+                TenantName  = $t.Name;
+                TenantCategory  = $t.Category;
+                TenantDomains  = $t.Domains;
+                SubId = $s.Id;
+                SubName = $s.Name;
+                SubState = $s.State;
+            };
+        }
+        else
+        {
+            $list += New-Object PSObject -Property @{
+                TenandId = $t.Id;
+                TenantName  = $t.Name;
+                TenantCategory  = $t.Category;
+                TenantDomains  = $t.Domains;
+                SubId = '---';
+                SubName = '---';
+                SubState = '---';
+                };
+                break;
+        }
+    }
+}
+
+#Print Tenant and Subscription in single line.
+$list | Select-Object -Property TenandId,TenantName,TenantCategory,TenantDomains,SubId,SubName,SubState | Format-Table -AutoSize
+
+
+
